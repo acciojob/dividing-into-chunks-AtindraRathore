a066ddc6d29a51e2999836b3e0eaf5c3b3afe832
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-13 15:49:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 const divide = (arr, n) => {
   // Write your code here
 	let subarray = [],ans=[],maxsum=0;
-	for(int i=0;i<arr.length;i++){
+	for(let i=0;i<arr.length;i++){
 		if(maxsum+arr[i] <= n){
 			maxsum+=arr[i];
 			subarray.push(arr[i]);
