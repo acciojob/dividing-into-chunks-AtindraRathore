b29558c9b7737124e5539804add7f9c8269b97ee
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-13 15:40:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let subarray = [],ans=[],maxsum=0;
+	for(int i=0;i<arr.length;i++){
+		if(maxsum+arr[i] <= n){
+			maxsum+=arr[i];
+			subarray.push(arr[i]);
+		}
+		else{
+			ans.push(subarray);
+			maxsum=0;
+			subarray.length = 0;
+		}
+	}
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// const n = prompt("Enter n: ");
+// alert(JSON.stringify(divide(arr, n)));
